24b839c3821dfaf84723ca2d3cda343b9705e647
Joe Meyer <josephmeyer03.jm@gmail.com>
2024-07-11 16:52:48 -0400

    Changing redirect uri again

--- src/script.js
+++ src/script.js
@@ -55,7 +55,7 @@ export async function getAccessToken(clientId, code) {
     params.append("client_id", clientId);
     params.append("grant_type", "authorization_code");
     params.append("code", code);
-    params.append("redirect_uri", "https://joemeyer03.github.io/spotify-web-app");
+    params.append("redirect_uri", "http://localhost:5173/callback");
     params.append("code_verifier", verifier);
 
     const result = await fetch("https://accounts.spotify.com/api/token", {
